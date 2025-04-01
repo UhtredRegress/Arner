42a989c9286a7324967858a789db34659a8cfbd7
Beanmodf <dominhtriet47@gmail.com>
2025-04-01 20:17:14 +0700

    Implement api to access Item table

---
+++ Arner.DataAccess/IRepository/IItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using Arner.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arner.DataAccess.IRepository
+{
+    public interface IItemRepository : IRepository<Item>
+    {
+        Task<Item?> GetById(int id);
+    }
+}
---
+++ Arner.DataAccess/ItemRepository.cs
@@ -0,0 +1,46 @@
+﻿using Arner.DataAccess.IRepository;
+using Arner.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arner.DataAccess
+{
+    public class ItemRepository : IItemRepository 
+    {
+        private readonly ArnerDbContext _context;
+        public ItemRepository(ArnerDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<Item> Add(Item t)
+        {
+            await _context.Items.AddAsync(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+
+        public async Task<Item> Delete(Item t)
+        {
+            _context.Items.Remove(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+
+        public async Task<Item?> GetById(int id)
+        {
+            return await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<Item> Update(Item t)
+        {
+            _context.Items.Update(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+    }
+}

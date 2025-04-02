db6776caa81d0b483acc6e469cca34f30b8e2d86
Beanmodf <dominhtriet47@gmail.com>
2025-04-02 15:36:24 +0700

    Create interface and implement class for Add method

---
+++ Arner.Service/IService/IItemService.cs
@@ -0,0 +1,14 @@
+﻿using Arner.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arner.Service.IService
+{
+    public interface IItemService : IService<Item>
+    {
+        public Task<Item> GetById(int id);
+    }
+}
---
+++ Arner.Service/ItemService.cs
@@ -0,0 +1,50 @@
+﻿using Arner.DataAccess.IRepository;
+using Arner.DataAccess.Models;
+using Arner.Helper.Exceptions;
+using Arner.Service.IService;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arner.Service
+{
+    public class ItemService : IItemService
+    {
+        private readonly IItemRepository _itemRepo;
+
+        public ItemService(IItemRepository itemRepo)
+        {
+            _itemRepo = itemRepo;
+        }
+
+        public async Task<Item> Add(Item entity)
+        {
+            var tempItem = await _itemRepo.GetByNameAndOwner(entity.Name, entity.UserId);
+            
+            if (tempItem != null)
+            {
+                throw new DuplicateDataException();
+            }
+        
+            return await _itemRepo.Add(entity); ;
+        }
+
+        public Task<Item> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Item> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Item> Update(int id, Item entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
